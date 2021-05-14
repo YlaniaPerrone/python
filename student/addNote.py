319ce454cf0e9697a7953d4e514fa8b74c4397f1
perrone <ylenia.icmr@gmail.com>
2021-05-14 22:42:52 +0200

    exercices

---
+++ student/addNote.py
@@ -0,0 +1,18 @@
+
+notes = []
+
+entree = None
+
+
+while entree != 'stop':
+
+    entree = input("Entrer une note : ")
+
+    if entree != 'stop':
+        
+    
+        notes.append(entree)
+    
+ 
+
+print(notes)
