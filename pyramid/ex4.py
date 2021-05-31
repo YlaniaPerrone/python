7ee52bbcd9b71b88778caf641ef885de888cd655
perrone <ylenia.icmr@gmail.com>
2021-05-31 20:59:17 +0200

    pyramid

--- pyramid/ex4.py
+++ pyramid/ex4.py
@@ -1,4 +1,6 @@
-n= int(input("Enter a num"))
+n= int(input("Enter a num (2 à 10) : "))
+while n > 10 or n < 2:
+    n= int(input("Enter a num (2 à 10) :"))
 r= (n*2)-2
 for i in range(1, n+1):
     print (" "*r, end=" ")
